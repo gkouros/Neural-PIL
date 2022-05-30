ad655245d68bd698b50ba75d6cc709075e9e5337
George Kouros <george.kouros.ece@gmail.com>
2022-05-30 12:24:52 +0200

    Update job requirements

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -9,7 +9,7 @@ conda activate neuralpil
 
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
-DIR=/users/visics/gkouros/projects/nerf-repos/NeRD-Neural-Reflectance-Decomposition/
+DIR=/users/visics/gkouros/projects/nerf-repos/Neural-PIL
 cd $DIR
 
 python3 train_neural_pil.py --datadir /esat/topaz/gkouros/datasets/nerf/$NAME --basedir logs/$NAME --expname $EXP --gpu $CUDA_VISIBLE_DEVICES --config configs/neural_pil/real_world.txt --spherify
